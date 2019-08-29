84cdab8a05a48b0eeb3067b5b0e754a93200b4b4
shawn <shawnantonucci2018@gmail.com>
2019-08-29 13:54:12 -0700

    setup and deployed

--- src/constants.js
+++ src/constants.js
@@ -1,56 +1,70 @@
-import { AppConfig } from 'blockstack'
+import { AppConfig } from "blockstack";
 
-export const appConfig = new AppConfig(['store_write', 'publish_data'])
+export const appConfig = new AppConfig(["store_write", "publish_data"]);
 export const ANIMALS = [
-  {
-    id: 'cat',
-    name: 'Cat',
-    superpower: 'Has 9 lives.'
-  },
-  {
-    id: 'dog',
-    name: 'Dog',
-    superpower: 'Catching frisbees.'
-  },
-  {
-    id: 'lion',
-    name: 'Lion',
-    superpower: 'Roaring loudly.'
-  }]
+    {
+        id: "cat",
+        name: "Cat",
+        superpower: "Has 9 lives."
+    },
+    {
+        id: "dog",
+        name: "Dog",
+        superpower: "Catching frisbees."
+    },
+    {
+        id: "lion",
+        name: "Lion",
+        superpower: "Roaring loudly."
+    },
+    {
+        id: "snake",
+        name: "Snake",
+        superpower: "Hissing...."
+    }
+];
 
 export const TERRITORIES = [
-  {
-    id: 'forest',
-    name: 'Forest',
-    superpower: 'Trees!'
-  },
-  {
-    id: 'tundra',
-    name: 'Tundra',
-    superpower: 'Let it snow!'
-  }
-]
+    {
+        id: "forest",
+        name: "Forest",
+        superpower: "Trees!"
+    },
+    {
+        id: "tundra",
+        name: "Tundra",
+        superpower: "Let it snow!"
+    },
+    {
+        id: "desert",
+        name: "Desert",
+        superpower: "Heat!"
+    }
+];
 
-export const ME_FILENAME = 'me.json'
-export const SUBJECTS_FILENAME = 'subjects.json'
-export const EXPLORER_URL = 'https://explorer.blockstack.org'
-
+export const ME_FILENAME = "me.json";
+export const SUBJECTS_FILENAME = "subjects.json";
+export const EXPLORER_URL = "https://explorer.blockstack.org";
 
 export const OTHER_KINGDOMS = [
-  {
-    app: 'https://animal-kingdom-1.firebaseapp.com',
-    ruler: 'larry.id'
-  },
-  {
-    app: 'http://localhost:3001',
-    ruler: 'larz.id'
-  },
-  {
-    app: 'https://decentralised-islands.netlify.com',
-    ruler: 'yannael_leborgne.id'
-  },
-  {
-      app: 'https://thirsty-jang-0c0a17.netlify.com',
-      ruler: 'ma1222042.id.blockstack'
-  }
-]
+    {
+        app: "https://animal-kingdom-1.firebaseapp.com",
+        ruler: "larry.id"
+    },
+    {
+        app: "http://localhost:3001",
+        ruler: "larz.id"
+    },
+    {
+        app: "https://decentralised-islands.netlify.com",
+        ruler: "yannael_leborgne.id"
+    },
+    {
+        app: "https://thirsty-jang-0c0a17.netlify.com",
+        ruler: "ma1222042.id.blockstack"
+    },
+    {
+        app: "https://animalkingdoms.netlify.com",
+        ruler: "moxiegirl.id.blockstack"
+    }
+];
